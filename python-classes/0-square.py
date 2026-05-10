a9a99c67acdcbe3b45f7906a8be95c937cf08c18
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-05-10 19:53:45 +0200

    Adding Python classes tasks

---
+++ python-classes/0-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module defines an empty Square class."""
+
+
+class Square:
+    """This class represents a square."""
+
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a Square class with a private size attribute."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new square with a size.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
