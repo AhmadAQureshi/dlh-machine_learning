e5bb8ceae39f578faa796f01c91b5991830aac4d
AhmadAQureshi <ahmad_abdullah92@hotmail.com>
2026-05-18 22:35:28 +0000

    Updating Linear Algebra task

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
+"""Module performs add,subtraction, multiplication and division"""
 import numpy as np
+
+
 def np_elementwise(mat1, mat2):
     """Performs element-wise addition, subtraction, multiplication, and division"""
     add = np.add(mat1, mat2)
--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module concatenates matrices using Numpy"""
 import numpy as np
 
 
