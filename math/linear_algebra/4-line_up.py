b964a12b3fb45cd84aa8a1a9d412e0069f93e7cf
AhmadAQureshi <ahmad_abdullah92@hotmail.com>
2026-05-18 23:03:12 +0000

    Updating Linear Algebra task

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""Module adds element-wise"""
+#!/usr/bin/python3
+"""This module contains a function that adds two 2D matrices."""
 
 
-def add_arrays(arr1, arr2):
+def add_matrices2D(mat1, mat2):
+    """Add two matrices element by element."""
     if len(arr1) != len(arr2):
         return None
     else:
