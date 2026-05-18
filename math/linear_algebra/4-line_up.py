0f07ba7074ef501c2689512c2bd54329a0f0a8a2
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-05-19 01:35:43 +0200

    Updating Linear Algebra task

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
-#!/usr/bin/python3
-"""This module contains a function that adds two 2D matrices."""
+"""Module for adding two arrays element-wise."""
 
 
-def add_matrices2D(mat1, mat2):
-    """Add two matrices element by element."""
+def add_arrays(arr1, arr2):
+    """Add two arrays element-wise and return a new list."""
     if len(arr1) != len(arr2):
         return None
-    else:
-        return [arr1[i] + arr2[i] for i in range(len(arr1))]
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
