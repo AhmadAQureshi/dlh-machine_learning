feeff9afaab8d2702976331f0cf4d48db9f5ab81
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-05-19 00:44:24 +0200

    Updating Linear Algebra task

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,29 +1,7 @@
-#!/usr/bin/env python3
-"""Element-wise matrix operations without NumPy."""
+#!/usr/bin/python3
+"""Element-wise matrix operations."""
 
 
 def np_elementwise(mat1, mat2):
-    """Return element-wise sum, difference, product and quotient."""
-    add = []
-    sub = []
-    mul = []
-    div = []
-
-    for i in range(len(mat1)):
-        add_row = []
-        sub_row = []
-        mul_row = []
-        div_row = []
-
-        for j in range(len(mat1[i])):
-            add_row.append(mat1[i][j] + mat2[i][j])
-            sub_row.append(mat1[i][j] - mat2[i][j])
-            mul_row.append(mat1[i][j] * mat2[i][j])
-            div_row.append(mat1[i][j] / mat2[i][j])
-
-        add.append(add_row)
-        sub.append(sub_row)
-        mul.append(mul_row)
-        div.append(div_row)
-
-    return add, sub, mul, div
+    """Return sum, difference, product and quotient of two matrices."""
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
