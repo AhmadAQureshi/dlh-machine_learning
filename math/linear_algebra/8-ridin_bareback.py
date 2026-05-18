b62576a159e44bed10e3fefd313dd4ebb99d98c3
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-05-19 01:19:30 +0200

    Updating Linear Algebra task

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""8-ridin_bareback.py"""
+"""Module for multiplying two 2D matrices."""
 
-import numpy as np
 
 def mat_mul(mat1, mat2):
-    """Multiplies two matrices using numpy"""
-    if np.array(mat1).shape[1] != np.array(mat2).shape[0]:
+    """Multiply two 2D matrices and return a new matrix."""
+    if len(mat1[0]) != len(mat2):
         return None
-    else:
-        mat_mul = np.matmul(mat1, mat2)
-    return np.matmul(mat1, mat2)
+
+    return [[sum(mat1[i][k] * mat2[k][j] for k in range(len(mat2)))
+             for j in range(len(mat2[0]))] for i in range(len(mat1))]
