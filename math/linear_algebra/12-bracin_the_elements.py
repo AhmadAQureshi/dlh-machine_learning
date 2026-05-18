239bf6dc9ffc26d2a9e3d2b424c4a52e1bc2b062
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-05-19 00:39:02 +0200

    Updating Linear Algebra task

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,12 +1,29 @@
-#!/usr/bin/env python3
-"""Module performs add,subtraction, multiplication and division"""
-import numpy as np
+#!/usr/bin/python3
+"""Element-wise matrix operations without NumPy."""
 
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise addition, subtraction, multiplication, and division"""
-    add = np.add(mat1, mat2)
-    sub = np.subtract(mat1, mat2)
-    mul = np.multiply(mat1, mat2)
-    div = np.divide(mat1, mat2)
+    """Return element-wise sum, difference, product and quotient."""
+    add = []
+    sub = []
+    mul = []
+    div = []
+
+    for i in range(len(mat1)):
+        add_row = []
+        sub_row = []
+        mul_row = []
+        div_row = []
+
+        for j in range(len(mat1[i])):
+            add_row.append(mat1[i][j] + mat2[i][j])
+            sub_row.append(mat1[i][j] - mat2[i][j])
+            mul_row.append(mat1[i][j] * mat2[i][j])
+            div_row.append(mat1[i][j] / mat2[i][j])
+
+        add.append(add_row)
+        sub.append(sub_row)
+        mul.append(mul_row)
+        div.append(div_row)
+
     return add, sub, mul, div
