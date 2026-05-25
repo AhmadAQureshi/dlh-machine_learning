a4a076442fdcf7a0f014b078c715c78dff8e19e0
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-05-25 14:52:08 +0200

    Wrong folders copied

--- python-data_structures/10-divisible_by_2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    else:
-        list_result = []
-        for i in range(len(my_list)):
-            list_result.append(my_list[i] % 2 == 0)
-        return list_result
