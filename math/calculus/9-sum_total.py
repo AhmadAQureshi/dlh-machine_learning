0bc9c0c5fe99c3bc3228ea374348aaa980f24e7c
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-05 01:53:25 +0200

    Updating calculus task
    Fix typo in docstring and remove extra whitespace.

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""Task9-calculating the sum of squares."""
-
+"""Task9-calculatin the sum of squares."""
 
 def summation_i_squared(n):
     """Return the sum of squares from 1 to n."""
     if type(n) is not int or n < 1:
         return None
-
     return sum(i ** 2 for i in range(1, n + 1))
