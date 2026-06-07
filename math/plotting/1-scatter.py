78cec6868a5d12a98dce29a8d357af5c9615d27c
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-08 00:04:48 +0200

    Updating Plotting task

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Task01-Scatter plot"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,16 +9,15 @@ def scatter():
     """Scatter plot"""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
+
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
-    plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, c='magenta', marker='.')
-    # Add axis labels and title
+
+    plt.scatter(x, y, c='magenta')
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.title('Men\'s Height vs Weight')
-    # Show the final plot
     plt.show()
 
 
