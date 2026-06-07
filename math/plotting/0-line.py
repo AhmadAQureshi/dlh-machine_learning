1fba53d966f048094f0375059594f0f014c162be
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-07 23:36:29 +0200

    Adding Plotting task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
 
+def line():
+
+    y = np.arange(0, 11) ** 3
+    plt.figure(figsize=(6.4, 4.8))
+    plt.plot(y, label='y = x^3')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.show()
+if __name__ == "__main__":
+    line()
