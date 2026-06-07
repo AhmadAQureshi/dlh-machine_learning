30636368151b740ccdf2a5549bf1e1f1c9e22016
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-07 23:50:44 +0200

    Updating Plotting task

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,6 +10,7 @@ def change_scale():
     r = np.log(0.5)
     t = 5730
     y = np.exp((r / t) * x)
+    
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y)
     plt.xlim(0, 28650)
@@ -17,6 +19,7 @@ def change_scale():
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of C-14')
     plt.show()
+
+
 if __name__ == "__main__":
-    
     change_scale()
