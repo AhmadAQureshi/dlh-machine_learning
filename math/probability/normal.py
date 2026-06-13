c51e772d9bac64f40109047180dc4e2170f66cc1
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-14 01:51:23 +0200

    Updating probability task

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -2,6 +2,9 @@
 """Task06-Initialize Normal"""
 
 
+from math import erf
+
+
 class Normal:
     """Represents a normal distribution"""
     def __init__(self, data=None, mean=0., stddev=1.):
@@ -19,17 +22,36 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = float((sum([(x - self.mean) ** 2 for x in data])
                                  / len(data)) ** 0.5)
-
+      
     def z_score(self, x):
-        """Calculates the z-score of a given x-value"""
+        """Calculating the z-score of a given x-value"""
         return (x - self.mean) / self.stddev
 
     def x_value(self, z):
-        """Calculates the x-value of a given z-score"""
+        """Calculating the x-value of a given z-score"""
         return (z * self.stddev) + self.mean
 
     def pdf(self, x):
-        """Calculates the value of the PDF for a given x-value"""
+        """Calculating the value of the PDF for a given x-value"""
         return (1 / (self.stddev * (2 * 3.141592653589793) ** 0.5)) * \
                (2.718281828459045 ** (-0.5 * ((x - self.mean)
                                               / self.stddev) ** 2))
+
+    def pdf(self, x):
+        """Calculating the value of the PDF for a given x-value"""
+        return (1 / (self.stddev * (2 * 3.141592653589793) ** 0.5)) * \
+               (2.718281828459045 ** (-0.5 * ((x - self.mean)
+                                              / self.stddev) ** 2))
+
+    def cdf(self, x):
+        """Calculating the value of the CDF for a given x-value"""
+        pi = 3.1415926536
+
+        z = (x - self.mean) / (self.stddev * (2 ** 0.5))
+
+        erf = (2 / (pi ** 0.5)) * (
+            z - (z ** 3) / 3 + (z ** 5) / 10 -
+            (z ** 7) / 42 + (z ** 9) / 216
+        )
+
+        return 0.5 * (1 + erf)
