4d5669320490d3e5802870633f9067bba75f7ba8
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-14 01:08:49 +0200

    Updating the probability task

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Task04-Exponential PDF"""
+"""Task05-Exponential CDF"""
 
 
 class Exponential:
@@ -22,3 +22,9 @@ class Exponential:
         if x < 0:
             return 0
         return self.lambtha * (2.7182818285 ** (-self.lambtha * x))
+    
+    def cdf(self, x):
+        """Calculating the value of the CDF for a given time period"""
+        if x < 0:
+            return 0
+        return 1 - (2.7182818285 ** (-self.lambtha * x))
