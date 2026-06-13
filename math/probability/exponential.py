d7d7abfe9f89a3f6c2cc7f75bfe0512a49b0098d
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-13 18:24:29 +0200

    Updating Probability task

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -11,8 +11,8 @@ class Exponential:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
         else:
-            if type(data) is not list:
-                raise ValueError("data must be a list")
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(1 / (sum(data) / len(data)))
