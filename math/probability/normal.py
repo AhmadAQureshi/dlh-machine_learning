6bc74a943dcfe06674bcc6d760aa26ae742c82da
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-06-14 01:40:44 +0200

    Updating Probability task

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -19,7 +19,7 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = float((sum([(x - self.mean) ** 2 for x in data])
                                  / len(data)) ** 0.5)
-     
+
     def z_score(self, x):
         """Calculates the z-score of a given x-value"""
         return (x - self.mean) / self.stddev
