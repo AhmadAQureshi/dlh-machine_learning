5b9a9a09304dbc63226bf7d9e218348f97113b84
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-08-09 09:15:57 +0200

    Updating task

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Reverse and transpose a pandas DataFrame."""
 
-import pandas as pd
-
 
 def flip_switch(df):
-    """Sort rows in reverse order and transpose the DataFrame."""
-    return df.sort_index(ascending=False).transpose()
+    """Reverse row order and transpose the DataFrame."""
+    return df.iloc[::-1].transpose()
