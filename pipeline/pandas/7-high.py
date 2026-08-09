ad2a7c62de058212d8630876060d34ba1988ad9e
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-08-09 09:17:13 +0200

    Updating task

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 """Sort a pandas DataFrame by its High column."""
 
-import pandas as pd
-
 
 def high(df):
     """Return the DataFrame sorted by High in descending order."""
