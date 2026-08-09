65849a5cca96218eeac6fdc9fa563a67395664ce
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-08-09 09:22:25 +0200

    Updating task

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Set the Timestamp column as the DataFrame index."""
 
-import pandas as pd
-
 
 def index(df):
-    """Return the DataFrame with Timestamp set as its index."""
+    """Return the DataFrame with Timestamp as its index."""
     return df.set_index("Timestamp")
