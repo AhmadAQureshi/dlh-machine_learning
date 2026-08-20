47886abf1866b5378d368ed5d752601c80a11023
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-08-20 11:24:51 +0200

    Updating task from Data Collection- API
    Updated rate limit reset time calculation to round up minutes.

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -10,7 +10,6 @@ import time
 
 if __name__ == "__main__":
     url = sys.argv[1]
-
     response = requests.get(url)
 
     if response.status_code == 404:
@@ -18,7 +17,8 @@ if __name__ == "__main__":
 
     elif response.status_code == 403:
         reset_time = int(response.headers["X-RateLimit-Reset"])
-        minutes = (reset_time - int(time.time())) // 60
+        seconds = reset_time - int(time.time())
+        minutes = (seconds + 59) // 60
         print("Reset in {} min".format(minutes))
 
     else:
