9807b73d2541e8eb90e281cea4a585e8128ce5d7
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-08-26 15:17:25 +0200

    Updating Bayesian Probability task

--- math/bayesian_prob/0-likelihood.py
+++ math/bayesian_prob/0-likelihood.py
@@ -10,7 +10,9 @@ def likelihood(x, n, P):
         raise ValueError("n must be a positive integer")
 
     if not isinstance(x, int) or x < 0:
-        raise ValueError("x must be an integer that is greater than or equal to 0")
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
 
     if x > n:
         raise ValueError("x cannot be greater than n")
@@ -19,7 +21,9 @@ def likelihood(x, n, P):
         raise TypeError("P must be a 1D numpy.ndarray")
 
     if np.any(P < 0) or np.any(P > 1):
-        raise ValueError("All values in P must be in the range [0, 1]")
+        raise ValueError(
+            "All values in P must be in the range [0, 1]"
+        )
 
     n_fact = 1
     x_fact = 1
