0d2fa945ab7bac40625002bb05769083a5f275ab
AhmadAQureshi <38467262+AhmadAQureshi@users.noreply.github.com>
2026-08-27 12:29:45 +0200

    Updating Plotting task
    Updated the docstring and added x-ticks to the histogram.

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""Plot a histogram of student grades."""
+"""Module for plotting a histogram of student grades."""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def frequency():
-    """Plot the frequency distribution of student grades."""
-
+    """Plot a histogram showing the frequency of student grades."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
     plt.figure(figsize=(6.4, 4.8))
 
     plt.hist(
@@ -23,6 +21,7 @@ def frequency():
     plt.ylabel("Number of Students")
     plt.title("Project A")
 
+    plt.xticks(np.arange(0, 101, 10))
     plt.xlim(0, 100)
     plt.ylim(0, 30)
 
